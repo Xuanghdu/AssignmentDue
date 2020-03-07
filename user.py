a8c50466aa52edeb233717df9a6520be3e42756a
Yunhao Qian <yunhaoqian@hotmail.com>
2020-03-07 17:08:47 -0500

    Format user.py

--- user.py
+++ user.py
@@ -1,14 +1,16 @@
 class User:
-    def __init__(self,username,password,groups=None):
-        self.user=username
-        self.password=password
-        self.groups=groups
+    def __init__(self, username, password, groups=None):
+        self.user = username
+        self.password = password
+        self.groups = groups
 
     def __str__(self):
-        return "The username is "+str(self.user)+". \nThe password is"+str(self.password)+". \nThe user belongs to the group"+str(self.groups)
+        return "The username is " + str(self.user) + \
+            ". \nThe password is"+str(self.password) + \
+            ". \nThe user belongs to the group" + str(self.groups)
 
-    def change_username(self,old,new):
-        self.user=new
+    def change_username(self, old, new):
+        self.user = new
 
-    def change_password(self,old,new):
-        self.password=new
+    def change_password(self, old, new):
+        self.password = new
