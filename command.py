b7579adafeecf79479d79b9427d5e1eb5e1e5b2b
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2020-03-07 22:14:00 -0500

    command modified

--- command.py
+++ command.py
@@ -1,10 +1,32 @@
+from base import Base
+from user import User
+# from task import Task
+from group import Group
 
-# tasks = []
-# users = ['Jason']
-# groups = []
+def check_username(database):
+    while True:
+        username = input('username: ')
+        if username in database['users']:
+            user = User(username,database['users'][username]['password'],)
+            break
+        else:
+            print('user_name not valid')
+    '''TODO: return user,username'''    
+
+def MATCH(password):
+    return password == passwords[0] 
+    # user.password_match(password)
+
+# '''目前预设以下值'''
+# database = {
+#     'Users':['Jason','Zuo']
+# }
+
 import json
 file_location = ""
 database =  json.loads(file_location)
+
+
 while True:
     print("Welcome to ...")
     print("1.register")
@@ -13,6 +35,7 @@ while True:
     choice = input("choose a number:\n")
 
     if choice == '1':
+        '''TODO: this part is currently malfunctional'''
         user_name = input('user_name: ')
         while True:
             password = input('password: ')
@@ -22,32 +45,31 @@ while True:
             else: break
             print('registering user...')
             '''code for registering required'''
+
     elif choice == '2':
-        user,username =check_user_name()
-        '''TODO: 
-            while not user.password_match(password):
-                    print('Access Denied, please try again.')
-                    print(' 1. Change username')
-                    print(' 2. Change password')
-                    change_user_name = input()
-                    if change_user_name == '1':
-                        user,username =check_user_name()
-                    elif change_user_name == '2':
-                        password = input('password: ')
-                    else:
-                        print('Please make a valid choice:')
-        '''
-            
+        '''Obtain the user and his relative groups and task'''
+        user,username =check_username()
+        #tasks
+        #group
+        ''''''
+
+
+
+        
+        while not MATCH():
+                print('Access Denied, please try again.')
+                print(' 1. Change username')
+                print(' 2. Change password')
+                change_user_name = input()
+                if change_user_name == '1':
+                    user,username =check_user_name()
+                elif change_user_name == '2':
+                    password = input('password: ')
+                else:
+                    print('Please make a valid choice:')
+        
+
     else:
         break
 
-def check_user_name():
-    while True:
-        user_name = input('user_name: ')
-        if user_name in database['users']:
-            '''TODO: user = User(???)'''
-            password = input('password: ')
-            break
-        else:
-            print('user_name not valid')
-    '''TODO: return user,username'''    
+
