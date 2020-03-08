35836badfd90718b3e0603fc7f74ac3ce48e784c
Yunhao Qian <yunhaoqian@hotmail.com>
2020-03-08 06:26:48 -0400

    More GUI.

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
 from tkinter import *
+from user import User
+from group import Group
+
+username_to_user = {}
 
 window = None
 navigation_stack = []  # user -> group -> task
@@ -40,17 +44,47 @@ def display_register_page():
     username = StringVar()
     password = StringVar()
     confirm_password = StringVar()
-    Label(text="Username:").pack()
-    Entry(textvariable=username).pack()
-    Label(text="Password:").pack()
-    Entry(textvariable=password).pack()
-    Label(text="Confirm password:").pack()
-    Entry(textvariable=confirm_password).pack()
-    Button(text="Login").pack()
+    Label(text="Username:").grid(row=0, column=0)
+    Entry(textvariable=username).grid(row=0, column=1)
+    Label(text="Password:").grid(row=1, column=0)
+    Entry(textvariable=password).grid(row=1, column=1)
+    Label(text="Confirm password:").grid(row=2, column=0)
+    Entry(textvariable=confirm_password).grid(row=2, column=1)
+    Button(text="Login").grid(row=3, column=0, columnspan=2)
+    window.mainloop()
+
+
+def display_user_page():
+    global window
+    assert(isinstance(navigation_stack[-1], User))
+    user = navigation_stack[-1]
+    if window != None:
+        window.destroy()
+    window = Tk()
+    window.title(user.username)
+    box = Listbox()
+    for group in user.groups:
+        box.insert(END, group.groupname)
+    box.pack()
+    window.mainloop()
+
+
+def display_group_page():
+    global window
+    assert(isinstance(navigation_stack[-1], Group))
+    group = navigation_stack[-1]
+    if window != None:
+        window.destroy()
+    window = Tk()
+    window.title(group.groupname)
+    box = Listbox()
+    for task in group.tasks:
+        box.insert(task.taskname)
+    box.pack()
     window.mainloop()
 
 
 if __name__ == "__main__":
     # display_login_or_register_page()
-    display_login_page()
-    # display_register_page()
+    # display_login_page()
+    display_register_page()
