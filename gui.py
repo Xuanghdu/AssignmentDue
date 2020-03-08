e1f9d3764dbefe07557f4d71378aef847b73eeb4
Yunhao Qian <yunhaoqian@hotmail.com>
2020-03-08 07:41:21 -0400

    More GUI.

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from user import User
 from group import Group
 
 username_to_user = {}
+groupname_to_group = {}
 
 window = None
 navigation_stack = []  # user -> group -> task
@@ -32,14 +33,14 @@ def display_login_page():
     Entry(textvariable=username).grid(row=0, column=1)
     Label(text="Password:").grid(row=1, column=0)
     Entry(textvariable=password).grid(row=1, column=1)
-    Button(text="Login", command=lambda: login_button_pressed(
+    Button(text="Login", command=lambda: login_pressed(
         username.get(), password.get())).grid(row=2, column=0)
     Button(text="Cancel", command=display_login_or_register_page).grid(
         row=2, column=1)
     window.mainloop()
 
 
-def login_button_pressed(username, password):
+def login_pressed(username, password):
     global username_to_user
     global navigation_stack
     if username == "" or password == "":
@@ -72,7 +73,7 @@ def display_register_page():
     Entry(textvariable=password).grid(row=1, column=1)
     Label(text="Confirm password:").grid(row=2, column=0)
     Entry(textvariable=confirm_password).grid(row=2, column=1)
-    Button(text="Register", command=lambda: register_button_pressed(
+    Button(text="Register", command=lambda: register_pressed(
         username.get(), password.get(), confirm_password.get())).grid(
             row=3, column=0)
     Button(text="Cancel", command=display_login_or_register_page).grid(
@@ -80,7 +81,7 @@ def display_register_page():
     window.mainloop()
 
 
-def register_button_pressed(username, password, confirm_password):
+def register_pressed(username, password, confirm_password):
     global username_to_user
     if password != confirm_password:
         messagebox.showinfo("Register failed",
@@ -115,8 +116,10 @@ def display_user_page():
                    row=row_count, column=0, columnspan=2)
         row_count += 1
     Button(text="Logout", command=logout_pressed).grid(row=row_count, column=0)
-    Button(text="Create group", command=create_group_pressed).grid(
-        row=row_count, column=1)
+    Button(text="Create group",
+           command=lambda: display_create_group_page(user)).grid(
+               row=row_count, column=1)
+    window.mainloop()
     window.mainloop()
 
 
@@ -127,12 +130,41 @@ def logout_pressed():
     display_login_or_register_page()
 
 
-def create_group_pressed():
-    pass
-
-
 def group_button_pressed(group):
-    pass
+    global navigation_stack
+    navigation_stack.append(group)
+    display_group_page()
+
+
+def display_create_group_page(user):
+    global window
+    global navigation_stack
+    assert(isinstance(navigation_stack[-1], User))
+    if window != None:
+        window.destroy()
+    window = Tk()
+    window.title("Create Group")
+    groupname = StringVar()
+    Label(text="Groupname").grid(row=0, column=0)
+    Entry(textvariable=groupname).grid(row=0, column=1)
+    Button(text="Create Group", command=lambda: create_group_pressed(
+        user, groupname.get())).grid(row=1, column=0)
+    Button(text="Cancel", command=display_user_page).grid(row=1, column=1)
+    window.mainloop()
+
+
+def create_group_pressed(user, groupname):
+    global navigation_stack
+    global groupname_to_group
+    assert(isinstance(navigation_stack[-1], User))
+    if groupname in groupname_to_group:
+        messagebox.showinfo("Create group failed",
+                            "Group name has been used by another group")
+        return
+    group = Group(groupname, [user])
+    groupname_to_group[groupname] = group
+    user.groups.append(group)
+    display_user_page()
 
 
 def display_group_page():
