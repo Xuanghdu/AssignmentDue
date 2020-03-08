79ed42050bb0205341f1dfd03b464d8783011095
Yunhao Qian <yunhaoqian@hotmail.com>
2020-03-08 05:35:18 -0400

    Add a __str__ method to task. Start to write GUI.

--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
-import tkinter as tk
+import tkinter
+
+window = None
+navigation_stack = [] # user -> group -> task
+
+def display_login_page():
+    window.distroy()
 
 if __name__ == "__main__":
-    ad = tk.Tk()
-    ad.title('hello')
+    ad = tkinter.Tk()
     button = tk.Button(ad, text='stop', width=25, command=ad.destroy)
     button.pack()
     ad.mainloop()
