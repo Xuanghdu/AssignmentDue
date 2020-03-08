2ada81b65baae2c8265a0499a64ede8a34601281
xuanghdu <wanggu52@ecf.utoronto.ca>
2020-03-07 19:09:07 -0500

    gui testing

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
-import tkinter
+import tkinter as tk
+
+if __name__ == "__main__":
+    ad = tk.Tk()
+    ad.title('hello')
+    button = tk.Button(ad, text='stop', width=25, command=ad.destroy)
+    button.pack()
+    ad.mainloop()
