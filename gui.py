b5b6a81a12de0f643cdfd1be0e72b313cfe7883a
Yunhao Qian <yunhaoqian@hotmail.com>
2020-03-08 06:15:38 -0400

    Edit task and more GUI.

--- gui.py
+++ gui.py
@@ -23,12 +23,34 @@ def display_login_page():
     window.title("Login")
     username = StringVar()
     password = StringVar()
-    Label("Username").pack()
+    Label(text="Username:").grid(row=0, column=0)
+    Entry(textvariable=username).grid(row=0, column=1)
+    Label(text="Password:").grid(row=1, column=0)
+    Entry(textvariable=password).grid(row=1, column=1)
+    Button(text="Login").grid(row=2, column=0, columnspan=2)
+    window.mainloop()
+
+
+def display_register_page():
+    global window
+    if window != None:
+        window.destroy()
+    window = Tk()
+    window.title("Register")
+    username = StringVar()
+    password = StringVar()
+    confirm_password = StringVar()
+    Label(text="Username:").pack()
     Entry(textvariable=username).pack()
-    Label("Password").pack()
+    Label(text="Password:").pack()
     Entry(textvariable=password).pack()
+    Label(text="Confirm password:").pack()
+    Entry(textvariable=confirm_password).pack()
+    Button(text="Login").pack()
+    window.mainloop()
 
 
 if __name__ == "__main__":
     # display_login_or_register_page()
     display_login_page()
+    # display_register_page()
